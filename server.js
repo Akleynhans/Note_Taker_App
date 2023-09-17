97895e6fe7c7cbb97072bfa71306ba9939becce6
Amy Kleynhans <amykleynhans@yahoo.com>
2023-09-17 11:06:20 -0600

    added initial variables

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// Helper method for generating unique ids
+// const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
