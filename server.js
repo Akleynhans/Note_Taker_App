e4d9434bdb979680dd6dce1019da00bb00aebfd2
Amy Kleynhans <amykleynhans@yahoo.com>
2023-09-17 12:08:25 -0600

    removed post request and added wildcard route

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -20,68 +21,17 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET request for notes
+// GET request for notes page
 app.get('/notes', (req, res) => {
     // Take to notes page
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 
-    // Log our request to the terminal
-    console.info(`${req.method} request received to get notes`);
 });
 
-
-// POST request to add a note
-app.post('/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a note`);
-
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-
-    // If all the required properties are present
-    if (title && text ) {
-        // Variable for the object we will save
-        const newNote = {
-            title,
-            text,
-           
-            // review_id: uuid(),
-        };
-
-        // Obtain existing notes
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-                // Convert string into JSON object
-                const parsednotes = JSON.parse(data);
-
-                // Add a new review
-                parsednotes.push(newNote);
-
-                // Write updated notes back to the file
-                fs.writeFile(
-                    './db/db.json',
-                    JSON.stringify(parsednotes, null, 4),
-                    (writeErr) =>
-                        writeErr
-                            ? console.error(writeErr)
-                            : console.info('Successfully updated notes!')
-                );
-            }
-        });
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting note');
-    }
-});
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
